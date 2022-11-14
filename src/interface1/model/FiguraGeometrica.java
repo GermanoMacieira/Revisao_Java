e19df5c625b4d7c6a52e533062ee79aefec3de66
GermanoMacieira <germanomacieira@hotmail.com>
2022-11-14 10:14:09 -0300

    Back-end: atualizando FiguraGeometrica.java, linha 5.

--- src/interface1/model/FiguraGeometrica.java
+++ src/interface1/model/FiguraGeometrica.java
@@ -2,6 +2,7 @@ package interface1.model;
 
 public interface FiguraGeometrica
 {
+    // Interface é uma classe abstrata com TODOS os métodos abstratos.
     // MÉTODOS A SEREM IMPLEMENTADOS:
     public String getNomeFigura();
     public int getArea();
